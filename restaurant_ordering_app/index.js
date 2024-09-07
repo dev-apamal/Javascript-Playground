785013658c13f829fc633703055eb7e288d71642
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-09-07 12:05:37 +0530

    commit -updated getMenu render

---
+++ restaurant_ordering_app/index.js
@@ -0,0 +1,30 @@
+import { menuArray } from './data.js'
+const feed = document.getElementById('feed')
+
+document.addEventListener('click', function(e){
+    console.log(e.target.id)
+})
+
+function getMenu(){
+    const menu = menuArray.map(function(items){
+        return `
+      <div class="itemContainer">
+        <div class="itemImageAndDetailsHolder">
+          <div class="itemImage"><img src="${items.image}" /></div>
+          <div class="itemDetails">
+            <h2>${items.name}</h2>
+            <p>${items.ingredients}</p>
+            <h3>${items.price}</h3>
+          </div>
+        </div>
+        <div class="plusBtn"><i class="fa-solid fa-plus" id="${items.id}"></i></div>
+      </div>`
+    }).join('')
+    return menu
+}
+
+function render(){
+    feed.innerHTML = getMenu()
+}
+
+render()
