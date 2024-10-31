0db163c493fbe21e454ee0dfd1f0e9a8588211d6
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-01 00:38:36 +0530

    feat(js): update functionality of 'get color scheme' button

--- colorscheme_generator/index.js
+++ colorscheme_generator/index.js
@@ -1,5 +1,8 @@
-const getColorSchemeButton = document.getElementById("getColorScheme") //buttons
+const seedColor = document.getElementById("seedcolor") //access seed color
+const getColorSchemeButton = document.getElementById("getColorScheme") //access colors button
+const colorTypes = document.getElementById("color-types") //access color scheme
 
 getColorSchemeButton.addEventListener('click', function(e){
-    console.log(e)
-})
+    console.log(seedcolor.value)
+    console.log(colorTypes.value)
+}) // get colors button listener
