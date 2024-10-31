31ac3e63c7b1c675b78b9a9a1925427de153df16
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-01 01:18:29 +0530

    feat(api): integrate backend logic for color scheme retrieval

--- colorscheme_generator/index.js
+++ colorscheme_generator/index.js
@@ -1,8 +1,27 @@
-const seedColor = document.getElementById("seedcolor") //access seed color
-const getColorSchemeButton = document.getElementById("getColorScheme") //access colors button
-const colorTypes = document.getElementById("color-types") //access color scheme
+const inputColor = document.getElementById("input-color") // access input color
+const seedcolor = document.querySelector(".seedcolor") // access seed color div
+const getColorSchemeButton = document.getElementById("getColorScheme") // access colors button
+const colorTypes = document.getElementById("color-types") // access different color types
+
+
 
 getColorSchemeButton.addEventListener('click', function(e){
-    console.log(seedcolor.value)
-    console.log(colorTypes.value)
+    // removes hash from the input color and gives a clean value.
+    function hashRemover(){
+        let hexOfInputColor = inputColor.value
+        let removeHash = hexOfInputColor.slice(1)
+        return removeHash
+    }
+    // stores the clean value in selectedColor variable
+    const selectedColor = hashRemover()
+
+    // Retrieve the selected color data from the Color API and apply it to the "seedcolor" div.
+    // While we could directly use inputColor.value to display the color, 
+    // I am making an API request here to learn/practice how API requests work.
+    fetch(`https://www.thecolorapi.com/id?hex=${selectedColor}`)
+        .then(res => res.json())
+        .then(data => {
+            seedcolor.style.background = `${data.hex.value}`
+        } 
+        )
 }) // get colors button listener
