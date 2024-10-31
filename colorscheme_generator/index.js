6984f3d1534480da6f36548e972b08bcf374b833
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-01 01:21:10 +0530

    chore: comments updated

--- colorscheme_generator/index.js
+++ colorscheme_generator/index.js
@@ -15,9 +15,9 @@ getColorSchemeButton.addEventListener('click', function(e){
     // stores the clean value in selectedColor variable
     const selectedColor = hashRemover()
 
-    // Retrieve the selected color data from the Color API and apply it to the "seedcolor" div.
-    // While we could directly use inputColor.value to display the color, 
-    // I am making an API request here to learn/practice how API requests work.
+    /* Retrieve the selected color data from the Color API and apply it to the "seedcolor" div.
+    While we could directly use inputColor.value and avoid all hastle to display the color, 
+    I am making an API request here just to learn/practice how API requests work.*/
     fetch(`https://www.thecolorapi.com/id?hex=${selectedColor}`)
         .then(res => res.json())
         .then(data => {
