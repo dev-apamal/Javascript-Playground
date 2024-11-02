8c92d5be22ba7040d90a6c0723fc5f98f102a02e
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-03 01:17:36 +0530

    refactor(js): updated efficiency(WIP)

--- colorscheme_generator/index.js
+++ colorscheme_generator/index.js
@@ -1,27 +1,31 @@
-const inputColor = document.getElementById("input-color") // access input color
-const seedcolor = document.querySelector(".seedcolor") // access seed color div
-const getColorSchemeButton = document.getElementById("getColorScheme") // access colors button
-const colorTypes = document.getElementById("color-types") // access different color types
+const inputColor = document.getElementById("input-color") 
+const getColorSchemeButton = document.getElementById("getColorScheme") 
+const colorTypes = document.getElementById("color-types")
+const footer = document.getElementById("footer")
+const colorPalette = document.getElementById("colorPalette")
 
+getColorSchemeButton.addEventListener('click', function(){
+    generateSchemeColors()
+}) // listen for any event on get color scheme.
 
+// removes hash from the input color and gives a clean value.
+function hashRemover(){
+    let hexOfInputColor = inputColor.value
+    let removeHash = hexOfInputColor.slice(1)
+    return removeHash
+}
 
-getColorSchemeButton.addEventListener('click', function(e){
-    // removes hash from the input color and gives a clean value.
-    function hashRemover(){
-        let hexOfInputColor = inputColor.value
-        let removeHash = hexOfInputColor.slice(1)
-        return removeHash
-    }
-    // stores the clean value in selectedColor variable
-    const selectedColor = hashRemover()
-
-    /* Retrieve the selected color data from the Color API and apply it to the "seedcolor" div.
-    While we could directly use inputColor.value and avoid all hastle to display the color, 
-    I am making an API request here just to learn/practice how API requests work.*/
-    fetch(`https://www.thecolorapi.com/id?hex=${selectedColor}`)
+// generate color scheme basis the input color and color type.
+function generateSchemeColors(){
+    let selectedColor = hashRemover()
+    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${colorTypes.value}&count=4`)
         .then(res => res.json())
         .then(data => {
-            seedcolor.style.background = `${data.hex.value}`
-        } 
-        )
-}) // get colors button listener
+            const colors = data.colors
+            for (let color of colors){
+                footer.innerHTML += `<div class="footer-hexcode">${color.hex.value}</div>`
+                colorPalette.innerHTML += `<div style="background: ${color.hex.value}"`
+                console.log(color.hex.value)
+            }
+        })
+}
