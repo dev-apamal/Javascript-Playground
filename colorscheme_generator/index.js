1beda26743b17dc57be7fd2d49d4cfe0136e4679
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-03 23:00:58 +0530

    feat(js): Code Refactor

--- colorscheme_generator/index.js
+++ colorscheme_generator/index.js
@@ -1,31 +1,32 @@
-const inputColor = document.getElementById("input-color") 
-const getColorSchemeButton = document.getElementById("getColorScheme") 
-const colorTypes = document.getElementById("color-types")
-const footer = document.getElementById("footer")
-const colorPalette = document.getElementById("colorPalette")
+const inputColor = document.getElementById("input-color") // Input Color
+const colorTypes = document.getElementById("color-types") // Color Scheme Types - monochrome, complement etc.
+const getColorSchemeButton = document.getElementById("getColorScheme") // Color Scheme Button
 
+const colorPalette = document.getElementById("colorPalette") // Generates color palette inside this.
+
+const footer = document.getElementById("footer") // Footer
+
+// Listen for any event on get color scheme button.
 getColorSchemeButton.addEventListener('click', function(){
     generateSchemeColors()
-}) // listen for any event on get color scheme.
+})
 
-// removes hash from the input color and gives a clean value.
+// Removes hash from the input color and gives a clean value.
 function hashRemover(){
     let hexOfInputColor = inputColor.value
     let removeHash = hexOfInputColor.slice(1)
     return removeHash
 }
 
-// generate color scheme basis the input color and color type.
+// Generate color scheme basis the input color and color type.
 function generateSchemeColors(){
     let selectedColor = hashRemover()
     fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${colorTypes.value}&count=4`)
         .then(res => res.json())
         .then(data => {
             const colors = data.colors
-            for (let color of colors){
-                footer.innerHTML += `<div class="footer-hexcode">${color.hex.value}</div>`
-                colorPalette.innerHTML += `<div style="background: ${color.hex.value}"`
-                console.log(color.hex.value)
+            for(let color of colors){
+                
             }
         })
 }
