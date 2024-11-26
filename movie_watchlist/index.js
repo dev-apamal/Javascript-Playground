0dd72335cabdeea4f5b39d3ed3f4b7bb0451de8a
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-26 22:56:12 +0530

    feat: add movie card component

---
+++ movie_watchlist/index.js
@@ -0,0 +1,4 @@
+const watchlistButton = document.getElementById('watchlistBtn')
+const searchField = document.getElementById('searchField')
+const searchButton = document.getElementById('searchButton')
+const resultContainer = document.getElementById('resultContainer')
