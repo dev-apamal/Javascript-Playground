4584f96eb1457caec70c795f0e68fd0cf4f43895
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-11-29 13:24:18 +0530

    api error - discarded

--- movie_watchlist/index.js
+++
@@ -1,4 +0,0 @@
-const watchlistButton = document.getElementById('watchlistBtn')
-const searchField = document.getElementById('searchField')
-const searchButton = document.getElementById('searchButton')
-const resultContainer = document.getElementById('resultContainer')
