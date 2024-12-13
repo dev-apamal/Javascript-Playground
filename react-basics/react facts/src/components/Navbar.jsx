97d9a3c2901a131136ddf7f9fdd9342ad4505781
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-12-13 12:15:37 +0530

    complete-header

---
+++ react-basics/react facts/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <header>
+            <nav>
+                <img src="./src/assets/react.svg" alt="Logo Image" className="logoImage"/>
+                <span className="logoText">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
