46e503f392f483b3f980b1cb993ec05feaa8697f
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-12-28 21:49:30 +0530

    feat ui: add responsiveness

--- meme_generator/my-project/src/components/Form.jsx
+++ meme_generator/my-project/src/components/Form.jsx
@@ -1,7 +1,7 @@
 export default function Form() {
   return (
-    <form className="flex justify-between">
-      <div className="flex flex-col gap-2">
+    <form className="flex justify-between flex-col sm:flex-row gap-4">
+      <div className="flex flex-col gap-2 w-full sm:w-1/2">
         <label for="top-text">Top Text</label>
         <input
           className="px-4 py-4 border border-gray-500 rounded"
@@ -10,7 +10,7 @@ export default function Form() {
           placeholder="Shut up"
         />
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 w-full sm:w-1/2">
         <label for="bottom-text">Bottom Text</label>
         <input
           className="px-4 py-4 border border-gray-500 rounded"
--- meme_generator/my-project/src/components/Meme.jsx
+++ meme_generator/my-project/src/components/Meme.jsx
@@ -1,5 +1,25 @@
 export default function Meme() {
   return (
-    <img src="./src/assets/image.png" alt="Meme" className="w-full rounded" />
+    <div className="meme relative text-center my-4 px-1.5">
+      <img src="./src/assets/image.png" alt="Meme" className="w-full rounded" />
+      <span
+        className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4 px-1.5 font-impact text-2xl uppercase text-white tracking-wide"
+        style={{
+          textShadow:
+            "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 0 2px 0 #000, 2px 0 0 #000, 0 -2px 0 #000, -2px 0 0 #000, 2px 2px 5px #000",
+        }}
+      >
+        Top Text
+      </span>
+      <span
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4 px-1.5 font-impact text-2xl uppercase text-white tracking-wide"
+        style={{
+          textShadow:
+            "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 0 2px 0 #000, 2px 0 0 #000, 0 -2px 0 #000, -2px 0 0 #000, 2px 2px 5px #000",
+        }}
+      >
+        Bottom Text
+      </span>
+    </div>
   );
 }
