3607cce5ddbfbb012437b7086390054311c2ef98
Amal A P <157817746+dev-apamal@users.noreply.github.com>
2024-12-31 11:48:31 +0530

    initial setup - add main element

--- tenzies/tenzies/src/App.jsx
+++ tenzies/tenzies/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl">Hello World</h1>;
+  return (
+    <main className="bg-customBlueGray w-full h-screen pt-6 pb-6 pr-6 pl-6">
+      <div className="bg-gray-100 h-full rounded-lg">Hi</div>
+    </main>
+  );
 }
